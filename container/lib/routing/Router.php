a3e8ae08bfec3fbb3be23a581d4b7eb90f7a6785
kohei.hujita <point.out92@gmail.com>
2020-10-05 03:18:06 +0900

    delete unnecessary method and prop

--- container/lib/routing/Router.php
+++ container/lib/routing/Router.php
@@ -2,17 +2,11 @@
 
 namespace Plum\Routing;
 
-use Plum\Foundation\Exceptions\NotMatchException;
+use Exception;
 use Plum\Http\Util\IMiddleware;
 use Plum\Foundation\Util\Reflect;
-use Plum\Routing\Exceptions\FileNotFoundException;
-use Plum\Routing\Exceptions\MissingSearchException;
-
-use Throwable;
 
 class Router implements IMiddleware {
-
-  protected $controller_file_path = '';
 
   protected $get_routers = [];
   protected $post_routers = [];
@@ -24,21 +18,11 @@ class Router implements IMiddleware {
    */
   public function run(): void
   {
-    $this->controller_file_path = '../app/controllers/' . $this->getControllerName() . '.php';
-
-    if (file_exists($this->controller_file_path)) {
-      include($this->controller_file_path);
-
-      if (request()->methodType() == "GET") {
-        $this->routerMapping($this->get_routers);
-      } else {
-        $this->routerMapping($this->post_routers);
-      }
+    if (request()->methodType() == "GET") {
+      $this->routerMapping($this->get_routers);
     } else {
-      throw new FileNotFoundException($this->controller_file_path);
-      // ViewUtil::dispNotFound();
+      $this->routerMapping($this->post_routers);
     }
-
   }
 
   /**
@@ -104,21 +88,6 @@ class Router implements IMiddleware {
   }
 
   /**
-   * get controller name. can get this by using namespace last string
-   *
-   * @return string
-   */
-  private function getControllerName(): string
-  {
-    if (request()->methodType() == "GET") {
-      $controller_name = $this->searchControllerName($this->get_routers);
-    } else {
-      $controller_name = $this->searchControllerName($this->post_routers);
-    }
-    return $controller_name;
-  }
-
-  /**
    * process and return router properties
    *
    * @param string $route
@@ -129,10 +98,10 @@ class Router implements IMiddleware {
   private function getProcessedRouter(string $route, string $controller, ?string $action = null): array
   {
     if (is_null($action)) {
-      $routing_props = explode('@', $controller);
+      $exploded = explode('@', $controller);
 
-      $controller = 'App\Controllers\\' . array_shift($routing_props);
-      $action = end($routing_props);
+      $controller = 'App\Controllers\\' . array_shift($exploded);
+      $action = end($exploded);
     }
 
     $router = [
@@ -164,39 +133,11 @@ class Router implements IMiddleware {
           $array_instance = Reflect::getArrayInstance($method_args);
 
           $response = $instance->$action(...$array_instance);
-        } catch (Throwable $th) {
-          echo $th->getMessage(), "\n";
+        } catch (Exception $e) {
+          throw $e;
         }
         echo $response;
       }
     }
-  }
-
-  /**
-   * searching controller name from `$routers`
-   *
-   * @param array $routers
-   * @return string
-   */
-  private function searchControllerName(array $routers): string
-  {
-    $controller_name = '';
-
-    foreach ($routers as $route => $value) {
-      if ($route === request()->getUriNoQuery()) {
-        $parse_namespace = explode('\\', $value['controller']);
-        $controller_name = end($parse_namespace);
-      }
-    }
-
-    if ($controller_name === '') {
-      if (empty($routers)) {
-        throw new MissingSearchException();
-      } else {
-        throw new NotMatchException('route');
-      }
-    }
-
-    return $controller_name;
   }
 }
