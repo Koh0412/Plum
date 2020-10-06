dbdb36b9d057847cc6ce9ac2a1ea4016a54e5147
kohei.hujita <point.out92@gmail.com>
2020-10-06 15:28:39 +0900

    add method get to RouteParameter class

--- container/lib/Routing/RouteParameters.php
+++ container/lib/Routing/RouteParameters.php
@@ -4,11 +4,26 @@ namespace Plum\Routing;
 
 class RouteParameters {
   public $params = [];
-  public function __construct(array $params) {
+  public function __construct(array $params)
+  {
     $this->params = $params;
 
     foreach ($params as $name => $value) {
       $this->$name = $value;
+    }
+  }
+
+  /**
+   * get route uri parameter by using `$param_name`
+   *
+   * @param string $param_name
+   * @return mixed
+   */
+  public function get(string $param_name)
+  {
+    $value = $this->params[$param_name];
+    if (isset($value)) {
+      return $value;
     }
   }
 }
